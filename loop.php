d8aae6e11146b5eae426ff9c57b9b8c71523c927
areejastal <101125082+areejastal@users.noreply.github.com>
2022-08-02 10:26:32 +0300

    Add files via upload

---
+++ loop.php
@@ -0,0 +1,39 @@
+<?php
+for($x = 0 ; $x<=100 ; $x+=10){
+    echo "number  : $x <br>";
+  }
+  
+  $a = [1 , 3 , 5];
+  for($i = 0 ;  $i <count($a) ; $i++){
+    echo $a[$i];
+  }
+  
+  
+  //foreach //array
+  $c = array('y' , 'w');
+  foreach($c as $value){
+    echo $value;
+  }
+  
+  $x= 1;
+  while($x <= 5){
+    echo "number  $x <br>";
+    $x++;
+  }
+  $x=1;
+  
+  do{
+    echo $x . "<br>";
+    $x++;
+    break;
+  }
+  while($x<=5);
+  
+  $age = array('v'=> '35');
+  foreach($age as $x=>$val){
+    echo $val;
+  }
+  
+  ?>
+  
+  
