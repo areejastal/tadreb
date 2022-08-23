2dc7af66e98e4aade801f5a9e9cea6841ee8a1c2
areejastal <101125082+areejastal@users.noreply.github.com>
2022-08-23 10:25:25 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,4 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "style.php";?>
+    <title>Document</title>
+</head>
+<body>
 
+<form action = "action.php? exampleInputEmail1= astalareej@gmail.com" method = "get">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value="areej@gmail.com">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+ 
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary" value="submit">Submit</button>
+</form>
 
-
-
+<?php
+include "JS.php";
+?>
+</body>
+</html>
